5fd4bc403924542f5a454c83423c2c5004cf6329
zaki <zaki.sediqyar@gmail.com.com>
2019-03-05 23:36:20 -0800

    working with handlebars

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express    =      require('express');
 const bodyParser =      require('body-parser');
+const expHand    =      require('express-handlebars');
 const mongoose   =      require('mongoose');
 const request    =      require('request');
 
 const app = express();
+
+var urls = ["https://www.google.com", "https://www.yahoo.com", "https://www.zsediqyar.com"];
 
 
 //setup
@@ -12,17 +15,45 @@ app.set('view engine', 'handlebars');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+function zaki() {
+    alert("noooo");
+}
 
 
-app.get('/', function(req, res) {
-    res.send("hello world");
+var result;
+
+function zaki() {
+    request('https://www.google.com', function(error, response, body) {
+
+        result = response.statusCode;
+
+        if(!error && response.statusCode == 200) {
+            document.write("The Site Is Up");
+            console.log(result);
+        } else {
+            console.log("code red");
+        }
+    });
+}
+
+
+// app.get("/", function (req, res, next) {
+//     let requestResult = zaki();
+//     console.log(requestResult);
+//     res.render("index", {status: requestResult});
+// });
+
+
+
+app.use(function (req, res) {
+    request("https://www.zsediqyar.com", function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            res.render("index", {status: "up"});
+        } else {
+            res.render("index", {status: "down"})
+        }
+    })
 });
-
-
-
-
-
-
 
 
 
